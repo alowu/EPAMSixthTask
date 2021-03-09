0a21be262e96cec6009fe0a2ac956a9479a6b5bd
alo_wu <ilya.bela@yandex.ru>
2021-03-09 19:09:56 +0300

    final

--- src/main/java/com/epam/task/sixth/Main.java
+++ src/main/java/com/epam/task/sixth/Main.java
@@ -6,10 +6,8 @@ import com.epam.task.sixth.entity.Players;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.log4j.Logger;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -20,7 +18,6 @@ import java.util.concurrent.Future;
 public class Main {
     private static final String INPUT_FILE = "src/main/resources/data.json";
     private static final Logger LOG = Logger.getLogger(Main.class);
-    private static final int CIRCLES_AMOUNT = 5;
 
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
         List<Player> players = readData(INPUT_FILE);
@@ -32,31 +29,26 @@ public class Main {
 
         List<Future<?>> futures = new ArrayList<>();
 
-        for (int i = 0; i < CIRCLES_AMOUNT; i++) {
-            players.stream().forEach(player -> {
-                Runnable r = new PlayerRunnable(player);
-                Future<?> future = executorService.submit(r);
-                futures.add(future);
-            });
 
-            for (Future<?> future : futures) {
-                future.get();
-            }
+        players.stream().forEach(player -> {
+            Runnable r = new PlayerRunnable(player);
+            Future<?> future = executorService.submit(r);
+            futures.add(future);
+        });
 
-            players.forEach(System.out::println);
-            System.out.println("-----------------------");
+        for (Future<?> future : futures) {
+            future.get();
         }
+
+        players.forEach(System.out::println);
 
         executorService.shutdown();
     }
 
     private static List<Player> readData(String inputFile) throws IOException {
-        try{
-            Path path = Paths.get(inputFile);
-            String data = String.join("\n", Files.readAllLines(path));
-
+        try {
             ObjectMapper objectMapper = new ObjectMapper();
-            Players playersWrapper = objectMapper.readValue(data, Players.class);
+            Players playersWrapper = objectMapper.readValue(new File(inputFile), Players.class);
             return playersWrapper.getPlayers();
         } catch (IOException e) {
             LOG.error(e.getMessage(), e);
--- src/main/java/com/epam/task/sixth/logic/PlayerLogic.java
+++ src/main/java/com/epam/task/sixth/logic/PlayerLogic.java
@@ -3,57 +3,46 @@ package com.epam.task.sixth.logic;
 import com.epam.task.sixth.entity.Player;
 import org.apache.log4j.Logger;
 
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class PlayerLogic {
     private final Player player;
 
     private static final Logger LOG = Logger.getLogger(PlayerLogic.class);
-    private static final Lock LOCK = new ReentrantLock();
 
     public PlayerLogic(Player player) {
         this.player = player;
     }
 
-    public void sellByn(int course){
-        try {
-            LOCK.lock();
-            int currentByn = player.getByn();
-            int bynToSell = currentByn / 2;
-            currentByn -= bynToSell;
+    public void sellByn(int course) {
 
-            int currentUsd = player.getUsd();
-            currentUsd += bynToSell / course;
+        int currentByn = player.getByn();
+        int bynToSell = currentByn / 2;
+        currentByn -= bynToSell;
 
-            player.setUsd(currentUsd);
-            player.setByn(currentByn);
-            LOG.info("player " + player.getId() + " sell byn " + bynToSell
-                    + " and now have " + currentUsd + " usd & " + currentByn + " byn" +
-                    " course " + course);
-        } finally {
-            LOCK.unlock();
-        }
+        int currentUsd = player.getUsd();
+        currentUsd += bynToSell / course;
+
+        player.setUsd(currentUsd);
+        player.setByn(currentByn);
+        LOG.info("player " + player.getId() + " sell byn " + bynToSell
+                + " and now have " + currentUsd + " usd & " + currentByn + " byn" +
+                " course " + course);
+
     }
 
-    public void sellUsd(int course){
-        try {
-            LOCK.lock();
-            int currentUsd = player.getUsd();
-            int usdToSell = currentUsd / 2;
-            currentUsd -= usdToSell;
+    public void sellUsd(int course) {
 
-            int currentByn = player.getByn();
-            currentByn += usdToSell * course;
+        int currentUsd = player.getUsd();
+        int usdToSell = currentUsd / 2;
+        currentUsd -= usdToSell;
 
-            player.setUsd(currentUsd);
-            player.setByn(currentByn);
-            LOG.info("player " + player.getId() + " sell usd " + usdToSell
-                    + " and now have " + currentUsd + " usd & " + currentByn + " byn" +
-                    " course " + course);
-        } finally {
-            LOCK.unlock();
-        }
+        int currentByn = player.getByn();
+        currentByn += usdToSell * course;
+
+        player.setUsd(currentUsd);
+        player.setByn(currentByn);
+        LOG.info("player " + player.getId() + " sell usd " + usdToSell
+                + " and now have " + currentUsd + " usd & " + currentByn + " byn" +
+                " course " + course);
+
     }
 }
